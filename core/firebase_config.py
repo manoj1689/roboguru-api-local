6a0ae010d3b0f7a516b3308b51c2236630c4cc1b
nasir <info@mobirizer.com>
2025-02-09 14:20:41 +0530

    Resolved merge conflicts

--- core/firebase_config.py
+++ core/firebase_config.py
@@ -4,11 +4,7 @@ from firebase_admin import credentials
 # Initialize Firebase Admin SDK
 def initialize_firebase():
     try:
-<<<<<<< HEAD
-        cred = credentials.Certificate("/home/nasir/workspace/eramlabs/robo_guru/roboguru_api/robo-guru-firebase-adminsdk-qch2e-a9083614a2.json")
-=======
         cred = credentials.Certificate("robo-guru-firebase-adminsdk-qch2e-a9083614a2.json")
->>>>>>> 69e201c15e1ea3e67d506f65ebbcef1ffa53f361
         firebase_admin.initialize_app(cred)
         print("Firebase Admin initialized successfully.")
     except ValueError:
