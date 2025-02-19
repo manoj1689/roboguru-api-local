b6f7b20d233b060a941bd725ddad29a25bc64be2
nikhil-mobirizer <nikhil@mobirizer.com>
2025-02-19 14:51:58 +0530

    update on config file

--- core/config.py
+++ core/config.py
@@ -1,17 +1,16 @@
 from pydantic_settings import BaseSettings
-from typing import ClassVar
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    SQLALCHEMY_DATABASE_URL: str
-    TRENDING_TOPICS_LIMIT: ClassVar[int] = 3
-    OPENAI_API_KEY: str
-    SUPERADMIN_UID: str
-
-    class Config:
-        env_file = ".env"
-        extra = "allow"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+    TRENDING_TOPICS_LIMIT: int = 3
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    SUPERADMIN_UID: str = os.getenv("SUPERADMIN_UID")
 
 settings = Settings()
